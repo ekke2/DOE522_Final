fd2a2aa80129b5db35da72d565737d9655c1f146
ekke2 <adriaancoetzee2004@gmail.com>
2024-08-15 23:01:24 +0200

    initial commit

---
+++ WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace DOE_FA1_Nico_Coetzee
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
